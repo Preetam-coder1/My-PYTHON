fed00c0349ff202b12d6e3611628bb899c389c30
Preetam Aich <preetamaich02@gmail.com>
2025-04-25 13:36:05 +0600

    Traffic.py
    Code of the traffic system.

---
+++ traffic.py
@@ -0,0 +1,10 @@
+light=input("light: ")
+print(light)
+if(light=="green"):
+    print("GO")
+elif(light=="yellow"):
+    print("READY")
+elif(light=="red"):
+    print("STOP")
+else:
+    print( "ERROR")
